d99fc1e6acac00477e26495f4f4d709484de8de1
Roger Li <rel7817@nyu.edu>
2023-12-27 13:00:31 -0500

    more updates

--- assets/domain_profile_url_regex.py
+++ assets/domain_profile_url_regex.py
@@ -1,3 +1,11 @@
+"""
+How to create Regex?
+    Escape . with \.
+    Replace numeric fields with (\d+)
+    Replace string fields with ([^/]+)
+    End regex with $
+"""
+
 domain_url_regex = {
     'www.cityandstateny.com': r'https://www\.cityandstateny\.com/voices/([^/]+)/(\d+)/?$',
     'www.columbiamissourian.com': r'https://www\.columbiamissourian\.com/users/profile/([^/]+)/?$',
@@ -6,5 +14,10 @@ domain_url_regex = {
     'www.commentary.org': r'https://www\.commentary\.org/author/([^/]+)/?$',
     'www.commondreams.org': r'https://www\.commondreams\.org/author/([^/]+)$',
     'rollcall.com': r'https://rollcall\.com/author/([^/]+)/$',
-    'www.somerset-kentucky.com': r'https://www\.somerset-kentucky\.com/users/profile/([^/]+)/$'
+    'www.somerset-kentucky.com': r'https://www\.somerset-kentucky\.com/users/profile/([^/]+)/$',
+    'www.cincinnati.com': r'https://www\.cincinnati\.com/staff/(\d+)/([^/]+)/$',
+    'www.milwaukiereview.com': r'https://www\.milwaukiereview\.com/users/profile/([^/]+)/$',
+    'cleantechnica.com': r'https://cleantechnica\.com/author/([^/]+)/$',
+    'www.clevelandbanner.com': r'https://www\.clevelandbanner\.com/users/profile/([^/]+)$',
+    'www.clevescene.com': r'https://www\.clevescene\.com/author/([^/]+)$',
 }
--- src/page_source_getter.py
+++ src/page_source_getter.py
@@ -10,13 +10,13 @@ import random
 class PageSourceGetter:
     def __init__(self):
         options = Options()
-        options.add_argument("--window-size=1280x720")
+        # options.add_argument('--headless')
+        # options.add_argument('--disable-gpu')
         # options.add_argument("--verbose")
+        options.add_argument("--window-size=1280x720")
         options.add_argument('--blink-settings=imagesEnabled=false')
         options.add_argument('--ignore-certificate-errors-spki-list')
         options.add_argument('--ignore-ssl-errors')
-        # options.add_argument('--headless')
-        # options.add_argument('--disable-gpu')
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--log-level=3")
         options.add_argument("--disable-notifications")
@@ -26,25 +26,8 @@ class PageSourceGetter:
         options.add_experimental_option('prefs', prefs)
         
         self.driver = webdriver.Chrome(options=options)
-        # network throttling.
-        # self.driver.set_network_conditions(
-        #     offline=False,
-        #     latency=5,  # additional latency (ms)
-        #     download_throughput=200 * 1024,  # maximal throughput
-        #     upload_throughput=200 * 1024   # maximal throughput
-        # )
         
         self.options = options
-    
-    # def get_page_source(self, url : str) -> str:
-    #     try:
-    #         self.driver.get(url)
-    #         time.sleep(random.uniform(0.6, 1.25))
-    #         return self.driver.page_source
-        
-    #     except Exception as e:
-    #         self.driver.close()
-    #         return ""
 
     def get_page_source(self, url: str, timeout: int = 10) -> str:
         try:
@@ -57,9 +40,8 @@ class PageSourceGetter:
             
             time.sleep(random.uniform(0.15, 0.3))
             return self.driver.page_source  
-        # except TimeoutException:
-        #     print(f"Timed out waiting for {url} to load")
         except Exception as e:
-            print(f"An error occurred: {e}")
+            # print(f"An error occurred: {e}")
+            print("A Selenium error occured...")
             self.driver.close()
             return ""
